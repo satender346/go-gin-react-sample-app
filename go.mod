e47ec311b09642629b606fa94fa7576a4979111b
Satender Yadav <satender346@gmail.com>
2021-10-02 23:27:53 -0500

    Add K8S deployment files

--- go.mod
+++ go.mod
@@ -2,7 +2,4 @@ module app
 
 go 1.14
 
-require (
-	github.com/gin-contrib/sessions v0.0.3
-	github.com/gin-gonic/gin v1.7.4
-)
+require github.com/gin-gonic/gin v1.7.4
--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
-go mod init app
-go build app
+#go mod init app
+#!/bin/bash
+go mod download
+go build -o app .
+sleep 30
 ./app
 
 echo "App is running, try localhost:7070 or <public-ip>:7070"
